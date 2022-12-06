a687e10caedc5f120b1366e996193038241a3bac
Schuylkill <schuylkill0@protonmail.com>
2022-12-05 20:58:29 -0500

    Add option to specify minimum age of messages to delete

--- discordsweep.py
+++ discordsweep.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 import requests
-from time import sleep
+import time
+import calendar
 
 banner = r'''
  (                           (                         
@@ -23,11 +24,12 @@ banner = r'''
 API_URL = "https://discordapp.com/api/v6/"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.9 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36"
 
-def retreiveMessages(serverID, userID, authToken, deleteCap=None):
+def retreiveMessages(serverID, userID, authToken, deleteCap=None, minAgeHours=None):
     deleteList = []
     offset = 0
-    lastLength = 0
     retryCap = 5
+    messageFound = False
+    now = calendar.timegm(time.gmtime())
     while True:
         if retryCap == 0:
             print(f"[ERROR] Retry cap ({retryCap}) hit, exiting...")
@@ -54,11 +56,11 @@ def retreiveMessages(serverID, userID, authToken, deleteCap=None):
                 dcResp = searchRes.json()
                 if "retry_after" in dcResp:
                     print(f"[WARNING] Being rate limited... Waiting {dcResp['retry_after']} ms")
-                    sleep(dcResp['retry_after'] / 1000)
+                    time.sleep(dcResp['retry_after'] / 1000 * 2)
                     continue
             except:
                 print("[WARNING] 429, too many requests! Waiting 30s...")
-                sleep(30)
+                time.sleep(30)
                 continue
         if searchRes.status_code != 200:
             print(f"[ERROR] Unexpected error {searchRes.status_code}")
@@ -69,25 +71,28 @@ def retreiveMessages(serverID, userID, authToken, deleteCap=None):
 
         if "retry_after" in dcResp:
             print(f"[WARNING] Being rate limited... Waiting {dcResp['retry_after']} ms")
-            sleep(dcResp['retry_after'] / 1000)
+            time.sleep(dcResp['retry_after'] / 1000 * 2)
             continue
 
         for messageBlock in dcResp["messages"]:
             for message in messageBlock:
                 # Only add to the delete list if the message author matches our user and isn't added already
                 if message["author"]["id"] == userID and not any(d["mid"] == message["id"] for d in deleteList):
-                    deleteList.append({
-                        "cid":message["channel_id"],
-                        "mid":message["id"]
-                    })
+                    messageFound = True
+                    timestamp = ((int(message["id"]) >> 22) + 1420070400000)/1000
+                    if minAgeHours is None or timestamp < now - minAgeHours*60*60:
+                        deleteList.append({
+                            "cid":message["channel_id"],
+                            "mid":message["id"]
+                        })
         
-        # If no messages have been added on the next page, assume no more are left
-        if len(deleteList) == lastLength:
+        # If no valid messages (ignoring timestamp) were found on the next page, assume no more are left
+        if not messageFound:
             return deleteList
 
         offset += 25
 
-        lastLength = len(deleteList)
+        messageFound = False
         print(f"[INFO] Current message count {len(deleteList)}")
 
     return deleteList
@@ -105,7 +110,7 @@ def deleteMessages(messages, authToken):
                 dcResp = deleteRes.json()
                 if "retry_after" in dcResp:
                     print(f"[WARNING] Being rate limited... Waiting {dcResp['retry_after']} ms")
-                    sleep(dcResp['retry_after'] / 1000)
+                    time.sleep(dcResp['retry_after'] / 1000 * 2)
                     continue
             except:
                 continue
@@ -115,12 +120,13 @@ parser.add_argument("serverID", help="Server ID to wipe from", type=str)
 parser.add_argument("userID", help="Your Discord user ID")
 parser.add_argument("authToken", help="Discord authorization token", type=str)
 parser.add_argument("-c", "--cap", help="Cap the amount of messages to delete", type=int)
+parser.add_argument("-a", "--age", help="Minimum age of messages to delete, in hours", type=int)
 args = parser.parse_args()
 
 print(banner)
 print(f"[INFO] Searching for messages from user {args.userID} in {args.serverID}")
 
-deleteList = retreiveMessages(args.serverID, args.userID, args.authToken, deleteCap=args.cap)
+deleteList = retreiveMessages(args.serverID, args.userID, args.authToken, deleteCap=args.cap, minAgeHours=args.age)
 print(f"[SUCCESS] Retreived all messages ({len(deleteList)}) from the server. Deleting...")
 deleteMessages(deleteList, args.authToken)
 print("[SUCCESS] Done!")
